0bad1346f981d32f2d9e13587c886e10ce9a608c
Albert Saiapin <analystalb@gmail.com>
2025-02-03 15:29:05 +0100

    blog_diag_jax function

--- source/matrix_operations.py
+++ source/matrix_operations.py
@@ -10,3 +10,19 @@ def khatri_rao_row(a: ArrayLike, b: ArrayLike) -> Array:
 def cpd_transform_vec(md, mi, mr):
     v = khatri_rao_row(mi.T, mr.T)
     return khatri_rao_row(md.T, v).T.sum(axis=1)
+
+@jit
+def block_diag_jax(matrices) -> ArrayLike:
+    """
+    Creates a block diagonal matrix from a list of matrices in JAX.
+    """
+    rows = sum(mat.shape[0] for mat in matrices)
+    cols = sum(mat.shape[1] for mat in matrices)
+    result = jnp.zeros((rows, cols))
+    r_offset, c_offset = 0, 0
+    for mat in matrices:
+        r, c = mat.shape
+        result = result.at[r_offset:r_offset+r, c_offset:c_offset+c].set(mat)
+        r_offset += r
+        c_offset += c
+    return result
