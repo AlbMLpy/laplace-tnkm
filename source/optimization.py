f785ca29af2b1d441ed51b518a8669a1e2d42f4a
Albert Saiapin <analystalb@gmail.com>
2025-02-26 15:51:10 +0100

    added adam optimizer

--- source/optimization.py
+++ source/optimization.py
@@ -7,6 +7,17 @@ def gd_update(w, dw, lr):
     w -= lr * dw
     return w
 
+def adam_update(w_vec, dw, m, v, t, lr, beta1=0.9, beta2=0.999, eps=1e-8):
+    t += 1  # Time step update
+    new_m = beta1*m + (1 - beta1)*dw
+    new_v = beta2*v + (1 - beta2)*(dw**2)
+    # Bias correction
+    m_hat = new_m / (1 - beta1**t) 
+    v_hat = new_v / (1 - beta2**t)
+    # Parameter update
+    new_params = w_vec - lr*m_hat/(jnp.sqrt(v_hat) + eps)
+    return new_params, new_m, new_v, t
+
 def std_transform(p):
     return jnp.log(jnp.exp(p) + 1)
 
