0a75e6e2629e638828aa7925c8112e021afbc79e
Albert Saiapin <analystalb@gmail.com>
2025-05-21 09:53:10 +0200

    Helping utility functions for optimization

--- source/optimization.py
+++ source/optimization.py
@@ -1,7 +1,11 @@
+from itertools import product
+
 import numpy as np
 import jax.numpy as jnp
+from jax import jit, hessian
 
-# Think about the functionality of this module?
+from .evaluation import l2_gb_loss
+from .matrix_operations import ten3tovec
 
 def gd_update(w, dw, lr):
     w -= lr * dw
@@ -24,3 +28,33 @@ def std_transform(p):
 def sample_w(w_mean, w_std):
     e_noise = jnp.array(np.random.randn(*w_mean.shape))
     return w_mean + e_noise*w_std
+
+def hess_full_jax(w_ten, kd, x, y, fmap, gamma_w, beta_e, w_shape):
+    hess_f = jit(hessian(l2_gb_loss, argnums=0), static_argnums=(4, 7))
+    w_vec = ten3tovec(w_ten)
+    hw = hess_f(w_vec, kd, x, y, fmap, gamma_w, beta_e, w_shape)
+    return hw
+
+def full_grid(params):
+    """
+    Full grid search on hyper parameters.
+    The function produces hyper parameters grid and names.
+    
+    Parameters
+    ----------
+    params : dict
+        Dictionary of parameters names and variable values,
+        e.g. {"A": [1, 2, 3], "B": [3, 4, 5]}.
+    
+    Returns
+    -------
+    grid, param_names : tuple
+        Tuple-like object, with the following attributes.
+    grid : set
+        Set of configurations.
+    param_names : tuple
+        Tuple of all the parameters names.
+    """
+    param_names, param_values = zip(*params.items())
+    grid = set(product(*param_values))
+    return grid, param_names 
