15e5432e96e52be4dc39379021f8fbd9f068f160
Albert Saiapin <analystalb@gmail.com>
2025-07-18 14:45:27 +0200

    New colors; New x3 plot;

--- source/visuals.py
+++ source/visuals.py
@@ -12,13 +12,22 @@ PARAMS = {
     'figure.constrained_layout.use': True,
     'figure.facecolor': 'white',
     'font.size': 10,
-    'axes.labelsize': 14,
-    'legend.fontsize': 12,
+    'axes.labelsize': 16,
+    'axes.grid': True,
+    'legend.fontsize': 16,
     'xtick.labelsize': 12,
     'ytick.labelsize': 12,
-    'axes.titlesize': 16,
+    'axes.titlesize': 18,
     'figure.max_open_warning': 50,
 }
+
+COLORS = dict(
+    red='#e60000',
+    green='#268c26', 
+    blue='#005ce6', 
+    orange='#ff7f0e', 
+    purple='#9467bd'
+)
 
 def plot_mtx(
     mtx, 
@@ -260,3 +269,74 @@ def generate_prediction_gif(
     for path in frame_paths:
         os.remove(path)
     os.rmdir(temp_dir)
+
+### Illustrative Example ###
+def plot_x3_results_multiple(
+    results,
+    data, 
+    figsize: Optional[tuple[int, int]] = None,
+    save_path: Optional[str] = None,
+    show_plot: bool = True,
+    y_limits: Optional[tuple] = None,
+    dpi: int = 500,
+):
+    pmean_color, pmean_linewidth, pmean_label = COLORS['red'], 2.0, r'Predictive Mean $\mu$'
+    pstd_color, pstd_alpha, pstd_label = pmean_color, 0.1, r'$\mu \pm 3 \sigma$'
+    mean_color, mean_linewidth, mean_label = COLORS['blue'], 2.0, r'True $\mathbb{E}[y]$'
+    train_marker, train_sz, train_color, train_label = '*', 40, mean_color, 'Train Data'
+    test_marker, test_sz, test_color, test_label = '+', 40, COLORS['green'], 'Test Data'
+    x_label, y_label, hm_pstd = 'x', 'y', 3
+
+    with mpl.rc_context(PARAMS):
+        num_models = len(results)
+        rows, cols = 1, num_models 
+        if figsize is None: figsize = (cols * 5, rows * 4)
+        fig, axes = plt.subplots(rows, cols, figsize=figsize, sharex=False, sharey=False)
+        axes = np.ravel(axes)
+        x_train, x_test, y_train, y_test, y_test_true = data
+        legend_handles, legend_labels = [], []
+        for i, (model_name, model_results) in enumerate(results.items()):
+            ax, pmean, pstd = axes[i], model_results['pred_mean'],  model_results['pred_std']
+            pmean_line, = ax.plot(
+                x_test, pmean, color=pmean_color, linewidth=pmean_linewidth, label=pmean_label,
+            )
+            pstd_area = ax.fill_between(
+                x_test[:, 0], (pmean - hm_pstd*pstd), (pmean + hm_pstd*pstd), 
+                color=pstd_color, alpha=pstd_alpha, label=pstd_label,
+            )
+            mean_line, = ax.plot(
+                x_test, y_test_true, color=mean_color, linewidth=mean_linewidth, label=mean_label,
+            )
+            train_scatter = ax.scatter(
+                x_train, y_train, marker=train_marker, s=train_sz, color=train_color, label=train_label,
+            )
+            test_scatter = ax.scatter(
+                x_test, y_test, marker=test_marker, s=test_sz, color=test_color, label=test_label,
+            )
+            ax.set_title(model_name)
+            ax.set_xlabel(x_label)
+            ax.set_ylabel(y_label)
+            if y_limits: 
+                ax.set_ylim(y_limits)
+            if i == 0:
+                legend_handles.extend([pmean_line, pstd_area, mean_line, test_scatter, train_scatter])
+                legend_labels.extend([pmean_label, pstd_label, mean_label, test_label, train_label])
+        for j in range(num_models, len(axes)):
+            fig.delaxes(axes[j])
+        legend = fig.legend(
+            handles=legend_handles,
+            labels=legend_labels,
+            loc='lower center',
+            bbox_to_anchor=(0.5, -0.1),
+            ncol=len(legend_handles),
+            frameon=True,
+        )
+        legend.get_frame().set_edgecolor('black')
+        legend.get_frame().set_linewidth(1.0)
+        plt.tight_layout(rect=[0, 0.05, 1, 1])  # leave space at bottom for legend
+        if save_path: 
+            plt.savefig(save_path, bbox_inches="tight", bbox_extra_artists=[legend], dpi=dpi)
+        if show_plot:
+            plt.show()
+        else:
+            plt.close()
