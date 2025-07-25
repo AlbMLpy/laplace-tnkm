e47374f1421591ba7ce4ad922f02a6c66b261966
Albert Saiapin <analystalb@gmail.com>
2025-07-25 20:04:11 +0200

    Added shifted features

--- train_uci.py
+++ train_uci.py
@@ -38,6 +38,7 @@ HESS_TYPE_HELP = "Choose hess_type:\
 FMAP_HELP = "Choose feature mapping:\
     'poly', 'poly_norm', 'fourier';"
 SCALE_HELP = "Choose float scale hyperparameter for 'fourier' fmap;"
+SHIFT_HELP = "Choose positive float shift hyperparameter for fmap;"
 SCORER_HELP = "Choose CV scorer function: 'll', 'nrmse';"
 TQDM_HELP = "Turn on/off tqdm interactive progress line;"
 
@@ -46,18 +47,17 @@ def argparse_uci():
     parser.add_argument('data', type=str, help=DATASET_HELP)
     parser.add_argument('hess', type=str, help=HESS_TYPE_HELP)
     parser.add_argument('fmap', type=str, help=FMAP_HELP)
-    parser.add_argument('-fs', '--f_scale', type=float, default=1.0, 
-                        help=SCALE_HELP)
-    parser.add_argument('-s', '--scorer', type=str, default='ll', 
-                        help=SCORER_HELP)
-    parser.add_argument('-tqdm', '--tqdm_enable', action="store_true", 
-                        help=TQDM_HELP)
+    parser.add_argument('-fs', '--f_scale', type=float, default=1.0, help=SCALE_HELP)
+    parser.add_argument('-fsh', '--f_shift', type=float, default=0.0, help=SHIFT_HELP)
+    parser.add_argument('-s', '--scorer', type=str, default='ll', help=SCORER_HELP)
+    parser.add_argument('-tqdm', '--tqdm_enable', action="store_true", help=TQDM_HELP)
     return parser.parse_args()
 
-def get_fmap(fmap, fs=None):
-    if fmap == 'poly': return PPFeature()
-    elif fmap == 'poly_norm': return PPNFeature()
-    elif fmap == 'fourier': return RBFFeature(l_scale=fs)
+def get_fmap(fmap, fs, shift):
+    sh = shift if shift > 0 else None
+    if fmap == 'poly': return PPFeature(shift=sh)
+    elif fmap == 'poly_norm': return PPNFeature(shift=sh)
+    elif fmap == 'fourier': return RBFFeature(l_scale=fs, shift=sh)
     else: raise ValueError()
 
 def get_scorer(scorer):
@@ -65,20 +65,22 @@ def get_scorer(scorer):
     elif scorer == 'nrmse': return nrmse_scorer
     else: raise ValueError()
 
+def get_res_dir(args):
+    fmap_str = f"{args.fmap}"
+    if args.fmap == 'fourier':
+        fmap_str += f"_ls{args.f_scale}"
+    if args.f_shift > 0:
+        fmap_str += f"_sh{args.f_shift}"
+    return Path(
+        ART_DIR / f'{args.data}/{args.hess}/{args.scorer}/{fmap_str}/training_artifacts/'
+    )
+
 if __name__ == '__main__':
     args = argparse_uci()
     data_path = DATA_DIR / f'{args.data}.csv'
     n_samples, d_dim = load_prepare_data(data_path, TEST_SIZE, None)[0].shape
-    spec_str = (
-        f"{args.fmap}_{args.scorer}" 
-        if args.fmap != 'fourier' 
-        else f"{args.fmap}{args.f_scale}_{args.scorer}"
-    )
-    res_dir = Path(
-        ART_DIR / f'{args.data}/{args.hess}/{spec_str}/training_artifacts/'
-    )
+    res_dir = get_res_dir(args)
     create_dir_if_not_exists(res_dir)
-
     tracker = CVTracker(res_dir, RES_NAME, CV_NAME, PRED_NAME)
     load_data_f = partial(
         load_transform_data, 
@@ -95,7 +97,7 @@ if __name__ == '__main__':
             d_dim,
             args.data, 
             args.hess, 
-            get_fmap(args.fmap, args.f_scale),
+            get_fmap(args.fmap, args.f_scale, args.f_shift),
             get_scorer(args.scorer),
             args.tqdm_enable,
         )
