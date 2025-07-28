0e618d2b496c84997146c97db2d5a8de21d632be
Albert Saiapin <analystalb@gmail.com>
2025-07-28 20:37:22 +0200

    No need in shift

--- full_uci.py
+++ full_uci.py
@@ -7,19 +7,18 @@ N_JOBS = 12
 
 def run(data_name, hess_type, fmap, shift):
     run_str = f"python train_uci.py {data_name} {hess_type} {fmap} -fsh {shift} -tqdm"
-    print(run_str)
     os.system(run_str)
 
 if __name__ == "__main__":
     data_name_list = ['yacht', 'energy', 'concrete', 'wine_red', 'kin8nm', 'naval', 'power', 'protein', 'boston']
     hess_type_list = ['last', 'block', 'gauss_newton']
-    fmap, fmap_shift = 'poly_norm', [0.0, 0.1]
-    options = list(product(data_name_list, hess_type_list, fmap_shift))
+    fmap, shift = 'poly_norm', 0.0
+    options = list(product(data_name_list, hess_type_list))
     if PARALLEL:
         Parallel(n_jobs=N_JOBS)(
             delayed(run)(data_name, hess_type, fmap, shift) 
-            for data_name, hess_type, shift in options
+            for data_name, hess_type in options
         )
     else:
-        for data_name, hess_type, shift in options:
+        for data_name, hess_type in options:
             run(data_name, hess_type, fmap, shift)
