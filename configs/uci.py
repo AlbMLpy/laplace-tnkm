6888c6cc6481561d739ec57681b14e20a86efabf
Albert Saiapin <analystalb@gmail.com>
2025-08-14 17:15:34 +0200

    Added more configs

--- configs/uci.py
+++ configs/uci.py
@@ -1,15 +1,20 @@
 from pathlib import Path
 from typing import Callable
 
-N_TRIALS = 10
-TEST_SIZE = 0.1
-MODEL_NAME = 'cpr_lla'
 DATA_DIR = Path('./data')
 ART_DIR = Path('./artifacts/uci')
+N_TRIALS, TEST_SIZE, DATA_SEED = 10, 0.1, 15
 TRANSFORM_X, TRANSFORM_Y, SCALER = False, True, 'std'
 RES_NAME, CV_NAME, PRED_NAME = 'results', 'cv_train', 'predictions'
 
-def get_exp_config(
+RANKS = [2, 4, 8, 16, 24, 32, 48, 64]
+M_ORDERS = [2, 4, 8, 16, 24, 32, 48, 64]
+HESS_THS = [1e-5, 1e-3, 1e-1]
+M_RANKS = [10, 25, 50]
+BETA_E, GAMMA_W, N_EPOCH_VI = None, 1e-5, 5
+CV_CONFIG = dict(n_splits=3, n_repeats=2, seed=1, n_jobs=1)
+
+def get_exp_config_la_btn(
     model_cls,
     n_samples: int,
     d_dim: int,
@@ -23,34 +28,69 @@ def get_exp_config(
         model_cls=model_cls,
         scaler=SCALER,
         par_fixed=dict(
-            fmap=fmap,
-            n_epoch=10, 
-            beta_e=None,
-            gamma_w=1e-5, # Decided to fix, otherwise CPD issue;
-            pd_mode='lla',
-            hess_type=hess_type,
-            seed=13,
-            n_epoch_vi=5,
-            pd_sample_seed=14,
+            fmap=fmap, n_epoch=10, beta_e=BETA_E, gamma_w=GAMMA_W,
+            seed=13, pd_mode='lla', hess_type=hess_type, pd_sample_seed=14,
+            n_epoch_vi=N_EPOCH_VI,
         ),
-        par_flexible=dict(
-            rank=[2, 4, 8, 16, 24, 32, 48, 64],
-            m_order=[2, 4, 8, 16, 24, 32, 48, 64],
-            hess_th=[1e-5, 1e-3, 1e-1],
-        ),
-        cv_config=dict(
-            n_splits=3,
-            n_repeats=2,
-            seed=1,
-            scorer=scorer,
-            n_jobs=1, # DO NOT CHANGE
-        ),
+        par_flexible=dict(rank=RANKS, m_order=M_ORDERS, hess_th=HESS_THS),
+        cv_config=CV_CONFIG | dict(scorer=scorer),
         data_config=dict(
-            data_path=DATA_DIR / f'{data_name}.csv',
-            n_samples=n_samples,
-            d_dim=d_dim,
-            test_size=TEST_SIZE,
-            seed=15,
+            data_path=DATA_DIR / f'{data_name}.csv', n_samples=n_samples,
+            d_dim=d_dim, test_size=TEST_SIZE, seed=DATA_SEED,
+        ),
+        tqdm_enable=tqdm_enable,
+    )
+
+def get_exp_config_mf_btn(
+    model_cls,
+    n_samples: int,
+    d_dim: int,
+    data_name: str, 
+    hess_type: str, 
+    fmap: tuple, 
+    scorer: Callable,
+    tqdm_enable: bool,
+) -> dict:
+    return dict(
+        model_cls=model_cls,
+        scaler=SCALER,
+        par_fixed=dict(
+            fmap=fmap, n_epoch=500, beta_e=BETA_E, gamma_w=GAMMA_W, 
+            seed=13, opt_params={'train_mode': 'adam', 'lr': 1e-3},
+            n_epoch_vi=N_EPOCH_VI,
+        ),
+        par_flexible=dict(rank=RANKS, m_order=M_ORDERS),
+        cv_config=CV_CONFIG | dict(scorer=scorer),
+        data_config=dict(
+            data_path=DATA_DIR / f'{data_name}.csv', n_samples=n_samples, 
+            d_dim=d_dim, test_size=TEST_SIZE, seed=DATA_SEED,
+        ),
+        tqdm_enable=tqdm_enable,
+    )
+
+def get_exp_config_sp_btn(
+    model_cls,
+    n_samples: int,
+    d_dim: int,
+    data_name: str, 
+    hess_type: str, 
+    fmap: tuple, 
+    scorer: Callable,
+    tqdm_enable: bool,
+) -> dict:
+    return dict(
+        model_cls=model_cls,
+        scaler=SCALER,
+        par_fixed=dict(
+            fmap=fmap, n_epoch=500, beta_e=BETA_E, gamma_w=GAMMA_W, 
+            seed=13, opt_params={'train_mode': 'adam', 'lr': 1e-3},
+            n_epoch_vi=N_EPOCH_VI,
+        ),
+        par_flexible=dict(rank=RANKS, m_order=M_ORDERS, m_rank=M_RANKS),
+        cv_config=CV_CONFIG | dict(scorer=scorer),
+        data_config=dict(
+            data_path=DATA_DIR / f'{data_name}.csv', n_samples=n_samples, 
+            d_dim=d_dim, test_size=TEST_SIZE, seed=DATA_SEED,
         ),
         tqdm_enable=tqdm_enable,
     )
