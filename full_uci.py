a37c4d7b93c21d37401b96acaabfb0bffb4a6b2d
Albert Saiapin <analystalb@gmail.com>
2025-08-14 17:16:09 +0200

    Added models: MF-BTN, SP-BTN

--- full_uci.py
+++ full_uci.py
@@ -1,24 +1,48 @@
 import os
-from itertools import product
+import argparse
+from itertools import product, chain
 from joblib import Parallel, delayed
 
-PARALLEL = True
 N_JOBS = 12
+PARALLEL = True
 
-def run(data_name, hess_type, fmap, shift):
-    run_str = f"python train_uci.py {data_name} {hess_type} {fmap} -fsh {shift} -tqdm"
+FMAP, FSHIFT = 'poly_norm', 0.0
+MODELS = ['la_btn', 'mf_btn', 'sp_btn']
+DATASETS = ['yacht', 'energy', 'concrete', 'wine_red', 'kin8nm', 'naval', 'power', 'protein', 'boston']
+HESS_TYPES = ['last', 'block', 'gauss_newton']
+
+MODEL_HELP = "Choose model: 'all', 'la_btn', 'mf_btn', 'sp_btn';"
+
+def argparse_uci():
+    parser = argparse.ArgumentParser(description='UCI experiment')
+    parser.add_argument('model', type=str, help=MODEL_HELP)
+    return parser.parse_args()
+
+def get_options(model, datasets, hess_types):
+    if model == 'la_btn':
+        return [(model, *v) for v in product(datasets, hess_types)]
+    elif model in MODELS:
+        return [(model, data, 'mf') for data in datasets]
+    else:
+        raise ValueError(f"Bad model name: {model}")
+
+def run(model_name, data_name, hess_type, fmap, shift):
+    run_str = f"python train_uci.py {model_name} {data_name} {hess_type} {fmap} -fsh {shift} -tqdm"
     os.system(run_str)
 
 if __name__ == "__main__":
-    data_name_list = ['yacht', 'energy', 'concrete', 'wine_red', 'kin8nm', 'naval', 'power', 'protein', 'boston']
-    hess_type_list = ['last', 'block', 'gauss_newton']
-    fmap, shift = 'poly_norm', 0.0
-    options = list(product(data_name_list, hess_type_list))
+    args = argparse_uci()
+    if args.model == 'all':
+        options = [get_options(model, DATASETS, HESS_TYPES) for model in MODELS]
+        options = list(chain.from_iterable(options))
+    else:
+        options = get_options(args.model, DATASETS, HESS_TYPES)
+    
     if PARALLEL:
         Parallel(n_jobs=N_JOBS)(
-            delayed(run)(data_name, hess_type, fmap, shift) 
-            for data_name, hess_type in options
+            delayed(run)(model_name, data_name, hess_type, FMAP, FSHIFT) 
+            for model_name, data_name, hess_type in options
         )
     else:
-        for data_name, hess_type in options:
-            run(data_name, hess_type, fmap, shift)
+        for model_name, data_name, hess_type in options:
+            run(model_name, data_name, hess_type, FMAP, FSHIFT)
