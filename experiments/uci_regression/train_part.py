95951dc2103772cb1cecb46f56cd259b2e43a4ec
Albert Saiapin <analystalb@gmail.com>
2026-01-29 11:39:17 +0100

    Added LA-BNN support

--- experiments/uci_regression/train_part.py
+++ experiments/uci_regression/train_part.py
@@ -9,6 +9,7 @@ sys.path.append(str(Path.cwd().parents[1]))
 import jax
 jax.config.update("jax_enable_x64", True)
 
+from source.models.LaplaceBNN import LaplaceBNN
 from source.models.LaplaceCPR import LaplaceCPR
 from source.models.MeanFieldBTN import MeanFieldBTN
 from source.models.StructPostBTN import StructPostBTN
@@ -21,6 +22,7 @@ from source.exp_functions import (
     nrmse_scorer,
     prepare_train_model,
     get_stats_several_trials, 
+    prepare_train_model_simple,
 )
 from configs.uci import ( 
     SCALER,
@@ -34,20 +36,21 @@ from configs.uci import (
     get_exp_config_la_btn,
     get_exp_config_mf_btn,
     get_exp_config_sp_btn,
+    get_exp_config_la_bnn,
 )
 
 DATA_DIR = Path.cwd().parents[1] / 'data'
 LOG_DIR = Path('./artifacts/logs')
 ART_DIR = Path('./artifacts/training_artifacts')
 
-MODEL_HELP = "Choose model: 'la_btn', 'mf_btn', 'sp_btn';"
+MODEL_HELP = "Choose model: 'la_btn', 'mf_btn', 'sp_btn', 'la_bnn';"
 DATASET_HELP = "Choose dataset:\
     'boston', 'concrete', 'energy', 'kin8nm', 'naval',\
     'power', 'protein', 'wine_red', 'yacht';"
 HESS_TYPE_HELP = "Choose hess_type:\
     'full', 'gauss_newton', 'block', 'mf', 'last';"
 FMAP_HELP = "Choose feature mapping:\
-    'poly', 'poly_norm', 'fourier';"
+    'poly', 'poly_norm', 'fourier', 'alt' (la_bnn);"
 SCALE_HELP = "Choose float scale hyperparameter for 'fourier' fmap;"
 SHIFT_HELP = "Choose positive float shift hyperparameter for fmap;"
 SCORER_HELP = "Choose CV scorer function: 'll', 'nrmse';"
@@ -59,10 +62,14 @@ def argparse_uci():
     parser.add_argument('data', type=str, help=DATASET_HELP)
     parser.add_argument('hess', type=str, help=HESS_TYPE_HELP)
     parser.add_argument('fmap', type=str, help=FMAP_HELP)
-    parser.add_argument('-fs', '--f_scale', type=float, default=1.0, help=SCALE_HELP)
-    parser.add_argument('-fsh', '--f_shift', type=float, default=0.0, help=SHIFT_HELP)
-    parser.add_argument('-s', '--scorer', type=str, default='ll', help=SCORER_HELP)
-    parser.add_argument('-tqdm', '--tqdm_enable', action="store_true", help=TQDM_HELP)
+    parser.add_argument(
+        '-fs', '--f_scale', type=float, default=1.0, help=SCALE_HELP)
+    parser.add_argument(
+        '-fsh', '--f_shift', type=float, default=0.0, help=SHIFT_HELP)
+    parser.add_argument(
+        '-s', '--scorer', type=str, default='ll', help=SCORER_HELP)
+    parser.add_argument(
+        '-tqdm', '--tqdm_enable', action="store_true", help=TQDM_HELP)
     return parser.parse_args()
 
 def get_fmap(fmap, fs, shift):
@@ -70,6 +77,7 @@ def get_fmap(fmap, fs, shift):
     if fmap == 'poly': return PPFeature(shift=sh)
     elif fmap == 'poly_norm': return PPNFeature(shift=sh)
     elif fmap == 'fourier': return RBFFeature(l_scale=fs, shift=sh)
+    elif fmap == 'alt': return None
     else: raise ValueError()
 
 def get_scorer(scorer):
@@ -84,7 +92,10 @@ def get_res_dir(args, dir_path=ART_DIR):
     if args.f_shift > 0:
         fmap_str += f"_sh{args.f_shift}"
     return Path(
-        dir_path / f'{args.model}/{args.data}/{args.hess}/{args.scorer}/{fmap_str}/'
+        (
+            dir_path 
+            / f'{args.model}/{args.data}/{args.hess}/{args.scorer}/{fmap_str}/'
+        )
     )
 
 def get_model_spec(model: str):
@@ -94,6 +105,8 @@ def get_model_spec(model: str):
         return MeanFieldBTN, get_exp_config_mf_btn
     elif model == 'sp_btn':
         return StructPostBTN, get_exp_config_sp_btn
+    elif model == 'la_bnn':
+        return LaplaceBNN, get_exp_config_la_bnn
     else:
         raise ValueError(f"Bad model name: {model}")
     
@@ -123,24 +136,20 @@ if __name__ == '__main__':
     create_dir_if_not_exists(res_dir)
     tracker = CVTracker(res_dir, RES_NAME, CV_NAME, PRED_NAME)
     load_data_f = partial(
-        load_transform_data, 
-        data_path=data_path,
-        test_size=TEST_SIZE,
-        transform_x=TRANSFORM_X,
-        transform_y=TRANSFORM_Y,
-        scaler=SCALER,
+        load_transform_data, data_path=data_path, test_size=TEST_SIZE,
+        transform_x=TRANSFORM_X, transform_y=TRANSFORM_Y, scaler=SCALER,
     )
     model_cls, config_f = get_model_spec(args.model)
-    train_model_f = prepare_train_model(
+    if args.model == 'la_bnn':
+        ptm_func = prepare_train_model_simple
+        jax.config.update("jax_enable_x64", False)
+    else:
+        ptm_func = prepare_train_model
+    train_model_f = ptm_func(
         config_f(
-            model_cls,
-            n_samples,
-            d_dim,
-            data_path, 
-            args.hess, 
+            model_cls, n_samples, d_dim, data_path, args.hess, 
             get_fmap(args.fmap, args.f_scale, args.f_shift),
-            get_scorer(args.scorer),
-            args.tqdm_enable,
+            get_scorer(args.scorer), args.tqdm_enable,
         ),
         logger=logger,
     )
